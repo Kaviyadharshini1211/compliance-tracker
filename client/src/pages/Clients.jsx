da354c5a49a367aec7547f2cf96c373f92dfb8ac
Kaviyadharshini1211 <kaviyadharshinim3@gmail.com>
2026-03-23 18:46:58 +0530

    implement client list UI

---
+++ client/src/pages/Clients.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const Clients = ({ setSelectedClient }) => {
+  const [clients, setClients] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/clients")
+      .then(res => setClients(res.data));
+  }, []);
+
+  return (
+    <div>
+      <h2>Clients</h2>
+
+      {clients.map(client => (
+        <div
+  key={client._id}
+  onClick={() => setSelectedClient(client._id)}
+  style={{
+    cursor: "pointer",
+    padding: "10px",
+    borderRadius: "6px",
+    marginBottom: "8px",
+    background: "#fff",
+    border: "1px solid #eee",
+    transition: "0.2s"
+  }}
+>
+  {client.company_name}
+</div>
+      ))}
+    </div>
+  );
+};
+
+export default Clients;
